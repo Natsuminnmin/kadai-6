45db88decc611103720223c12e03a3daee0f8177
Natsuminmin <basnatubadlove_misuse@outlook.jp>
2024-03-12 00:03:41 +0900

    アプリ作成した

---
+++ src/test/java/com/demodemo/demodemo/DemodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demodemo.demodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
