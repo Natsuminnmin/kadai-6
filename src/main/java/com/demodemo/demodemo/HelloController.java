e010e48e5f698278f85f074d85197556981becac
Natsuminmin <basnatubadlove_misuse@outlook.jp>
2024-03-12 23:54:54 +0900

    helloをfruit一覧に変更した

--- src/main/java/com/demodemo/demodemo/HelloController.java
+++ src/main/java/com/demodemo/demodemo/HelloController.java
@@ -3,11 +3,21 @@ package com.demodemo.demodemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
-@GetMapping("/hello")
-    public String hello(){
-    return  "Hello world!";
+@GetMapping("/fruit")
+public List<fruit> fruit(){
+    List<fruit> fruits = List.of (
+    new fruit("apple",1,200),
+    new fruit("banana", 5, 99),
+    new fruit("strawberry", 2, 400),
+    new fruit("avocado", 3, 100),
+    new fruit("orange", 4, 150));
+return fruits;
 }
 }
---
+++ src/main/java/com/demodemo/demodemo/fruit.java
@@ -0,0 +1,30 @@
+package com.demodemo.demodemo;
+
+public class fruit {
+
+private String name;
+
+private int ranking;
+
+private int price;
+
+public fruit(String name, int ranking, int price) {
+    this.name = name;
+            this.ranking = ranking;
+                    this.price= price;
+
+
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
